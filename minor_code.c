d9ddbdce70cb2811cbd54444643b2eaa88f681a8
cptn3m0grv <root@localhost.localdomain>
2020-09-14 11:54:50 +0530

    added function to block IP, but not yet complete

--- minor_code.c
+++ minor_code.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
 void colorRed() { printf("\033[1;31m"); }
 
@@ -8,6 +9,47 @@ void colorReset() { printf("\033[0m;"); }
 void colorYellow() { printf("\033[0;33m"); }
 
 void colorGreen() { printf("\033[0;32m"); }
+
+void ipBlockUnblock(char voiceChoice){
+
+	// Variable declaration
+	int v = 0;
+        char command[] = "firewall-cmd --permanent --add-rich-rule=\"rule family='ipv4' source address='";
+	char IP[20];
+	char tail[] = "' accept\"";
+	int status;
+
+	// Asking for the IP to block
+	if(voiceChoice == 'y') v = 1;
+	if(v) system("espeak-ng -s 140 -p 60 \"Enter the Ip to block\"");
+	printf("Enter the IP to block: ");
+        scanf("%20s", IP);
+        
+	// concatenating the command
+        strcat(command, IP);
+        strcat(command, tail);
+
+	// blocking the IP
+        status = system(command);
+
+	// throwing an error if IP blocking was failed
+	if(status != 0 ){
+		if(v) system("espeak-ng \"There is some error during the process\"");
+		printf("Blocking IP not successful");
+		exit(EXIT_FAILURE);
+	}
+
+	// reload the firewall rules to complete the process
+        system("firewall-cmd --reload");
+        system("firewall-cmd --list-all");
+
+
+	if(v) system("espeak-ng \"Ip blocked successfully\"");
+	colorGreen();
+	printf("IP blocked successfully.");
+	colorReset();
+}
+
 
 void convertToLog(char voiceChoice){
 	
@@ -134,7 +176,8 @@ int main(){
 	printf("\n\n\n");
 	printf("1. To covert log file into CSV file.\n");
         printf("2. To search for a occurence of a word in log file.\n");
-	
+	printf("3. Block / Unblock a particular IP using firewall.\n");
+
 	// take input for choice from above provided options
         printf("\nEnter your choice: ");
         scanf("%d", &choice);
@@ -148,6 +191,10 @@ int main(){
           case 2:
             searchForKeyword(voiceChoice);
             break;
+
+	  case 3:
+	    ipBlockUnblock(voiceChoice);
+	    break;
 
           default:
 	    if(voiceChoice == 'y'){ system("espeak-ng \"Not a valid option\""); }
